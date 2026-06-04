035fcad197a12c494d676e1476785744b9716e96
Gaurav Kumar Sinha <gaurav@substrai.dev>
2026-06-04 08:24:06 -0500

    chore: bump version to 1.0.0 for PyPI release
    Signed-off-by: Gaurav Kumar Sinha <gaurav@substrai.dev>

--- src/agentdeploy/__init__.py
+++ src/agentdeploy/__init__.py
@@ -13,7 +13,7 @@ Usage:
         return "Hello!"
 """
 
-__version__ = "0.9.0"
+__version__ = "1.0.0"
 
 from agentdeploy.core.agent import agent, AgentConfig
 from agentdeploy.core.runtime import AgentRuntime, InvocationResult
